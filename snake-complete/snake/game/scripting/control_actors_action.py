cbeb90aa920dc81eb66b82b2447ea8362ee346a2
TicTacToe333 <aidencedwards@gmail.com>
2022-03-02 09:49:17 -0700

    You can't move back on yourself anymore

--- snake-complete/snake/game/scripting/control_actors_action.py
+++ snake-complete/snake/game/scripting/control_actors_action.py
@@ -2,6 +2,11 @@ import constants
 from game.scripting.action import Action
 from game.shared.point import Point
 
+LEFT = Point(-constants.CELL_SIZE, 0)
+RIGHT = Point(constants.CELL_SIZE, 0)
+UP = Point(0, -constants.CELL_SIZE)
+DOWN = Point(0, constants.CELL_SIZE)
+
 
 class ControlActorsAction(Action):
     """
@@ -30,20 +35,20 @@ class ControlActorsAction(Action):
             script (Script): The script of Actions in the game.
         """
         # left
-        if self._keyboard_service.is_key_down('a'):
-            self._direction = Point(-constants.CELL_SIZE, 0)
+        if self._keyboard_service.is_key_down('a') and self._direction != RIGHT:
+            self._direction = LEFT
         
         # right
-        if self._keyboard_service.is_key_down('d'):
-            self._direction = Point(constants.CELL_SIZE, 0)
+        if self._keyboard_service.is_key_down('d') and self._direction != LEFT:
+            self._direction = RIGHT
         
         # up
-        if self._keyboard_service.is_key_down('w'):
-            self._direction = Point(0, -constants.CELL_SIZE)
+        if self._keyboard_service.is_key_down('w') and self._direction != DOWN:
+            self._direction = UP
         
         # down
-        if self._keyboard_service.is_key_down('s'):
-            self._direction = Point(0, constants.CELL_SIZE)
+        if self._keyboard_service.is_key_down('s') and self._direction != UP:
+            self._direction = DOWN
         
         snake = cast.get_first_actor("snakes")
         snake.turn_head(self._direction)
